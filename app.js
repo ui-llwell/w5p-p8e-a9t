91abcc11f065263ed1ca6f95879c3f6c59f6482b
Una <una_bella@qq.com>
2018-09-05 16:33:18 +0800

    merge
    about #3

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 import locales from './utils/locales'
 import T from './utils/i18n'
 
+
+
+
+// here
+
+
 T.registerLocale(locales)
 T.setLocaleByIndex(wx.getStorageSync('langIndex') || 0);
 wx.T = T
